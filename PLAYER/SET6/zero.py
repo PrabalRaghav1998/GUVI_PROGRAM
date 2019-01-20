ba042d68edfd4921c59dd16ad2b778d37739b2b6
Prashant Raghav <prashantraghav74@gmail.com>
2019-01-20 21:21:22 +0530

    Update zero.py

--- PLAYER/SET6/zero.py
+++ PLAYER/SET6/zero.py
@@ -1,8 +1,7 @@
-n=input()
-s=""
-while(len(n)!=""):
-    p=n.index("0")
-    l=n[:p+1]
-    s+=" ".join(l)
-    n.remove(l)
-print(s)
+n1=int(input())
+l=list(map(int,input().split()))
+l=l[::-1]
+i=l.index(0)
+for j in range(2,n1):
+    if l[j]!=0:
+        print(l[j],end=" ")
