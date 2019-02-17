2c6616a0e6731cdb422bab3e125257dfb8c8efd5
Prashant Raghav <prashantraghav74@gmail.com>
2019-02-17 18:47:29 +0530

    Add files via upload

---
+++ PRO/SET1/prefix.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=[]
+for i in range(n):
+    l.append(input())
+l.sort()
+a,b=l[0],l[-1]
+l1=[]
+for i in range(len(a)):
+    if a[i]==b[i]:
+        l1.append(a[i])
+    else:
+        break
+print("".join(l1))
